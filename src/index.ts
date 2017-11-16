28abcbc730c16e70a67d6df97ba107b84973cb52
michaelolof <myqrrlolof@yahoo.com>
2017-11-16 19:47:55 +0100

    changed mixins to start accepting classes insted of objects

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,43 @@
-function mix( baseClass: any, mixinObjects: object[] ) {
-  // We don't want to override methods or properties that are explicitly defined in the base classes.
-  const baseClassMethodNames: string[] = Object.getOwnPropertyNames( baseClass.prototype )
-  const baseClassMethodsMinusConstructor = baseClassMethodNames.slice( 1, baseClassMethodNames.length ); // Don't mess with the constructor.
-  const baseClassPropertyNames: string[] = Object.getOwnPropertyNames( new baseClass() )
-  const baseClassNames = [...baseClassPropertyNames, ...baseClassMethodsMinusConstructor ];
-  const mergedMixinObject:object = Object.assign( {}, ...mixinObjects );
-  
-  for(let mixinProp in mergedMixinObject ) {
-    if( baseClassMethodsMinusConstructor.indexOf( mixinProp ) > - 1 ) return
-    const mixinValue = mergedMixinObject[mixinProp];
-    if( !( typeof mixinValue === "string" || typeof mixinValue === "number" || 
-           typeof mixinValue === "function" || typeof mixinValue === "undefined" )) {
-      throw new Error("Defined mixin properties must be either 'string', 'number', 'function', or 'undefined'.\nAvoid declaring mixin properties of complex types like 'objects', 'classes' or 'null'.\nThis ability should be left to the concrete class utilizing the mixin.")
-    }
-    baseClass.prototype[ mixinProp ] = mixinValue;
-  }
+export type Constructor<T> = new (...args: any[]) => T;
+
+
+function mix( baseClass: Constructor<any>, mixinClasses: Constructor<any>[], deepMix?:boolean) {
+  if( deepMix == undefined ) deepMix = false;
+ 
+  // We don't want to override the constructor
+  // We also don't want to override methods or properties that are explicitly defined in the derived constructor.
+  mixinClasses.forEach( mixinClass => {
+    const basePropertyNames: string[] = Object.getOwnPropertyNames( baseClass.prototype )
+    const basePropertyMinusConstructor = basePropertyNames.slice( 1, basePropertyNames.length ); // Don't mess with the constructor.
+    
+    
+    // By default mixins shouldn't have a constructor but since a TypeScript mixin is just a class
+    // a default constructor is created for you. So we remove that.
+    const mixinPropertyNames = Object.getOwnPropertyNames( mixinClass.prototype );
+    const mixinPropertyMinusConstructor = mixinPropertyNames.slice( 1, mixinPropertyNames.length );
+
+    const keys = Object.keys( mixinClass.prototype );
+
+    const mixinClassObject = Object.create( baseClass.prototype );
+
+
+    mixinPropertyMinusConstructor.forEach( baseName => {
+      if( deepMix ) {
+        // const deepMixSymbol = `${mixinClass.name}.${baseName}`;
+        // baseClass.prototype[ deepMixSymbol ] = mixinClass.prototype[ baseName ];
+        baseClass.prototype[ mixinClass.name ] = mixinClassObject;
+        console.log( mixinClass.name )      
+      }
+      if( basePropertyMinusConstructor.indexOf( baseName ) > - 1 ) return
+      baseClass.prototype[ baseName ] = mixinClass.prototype[ baseName ];
+    })
+
+  });
 }
 
-export default function use(...options:object[] ) {
+// Decorators
+export default function use(...options:Constructor<{}>[] ) {
   return function ( target:any, propertyKey:string ) {
-    mix( target.constructor, options );
+    mix( target.constructor, options, true );
   }
 }
